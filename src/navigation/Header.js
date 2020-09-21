7e379012788af4ae07362acdad01442a5c3a8eca
geeuho <geeuho@gmail.com>
2020-09-21 13:50:46 -0700

    deleted old state functions

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -2,22 +2,12 @@ import React from 'react'
 import {Appbar} from 'react-native-paper'
 
 const Header = props => {
-    const [visible, setVisible] = React.useState(false);
-
-    let goBack = () =>  console.log("Went back")
-    let handleMenu = () => {
-        if(!visible){
-            setVisible(true)
-        } else {
-            setVisible(false)
-        }
-        console.log(visible)
-    }
+    
     return (
         <Appbar.Header>
-            <Appbar.Action icon="menu" onPress={handleMenu}/> 
+            <Appbar.Action icon="menu"/> 
             <Appbar.Content title = "Title"/>
-            <Appbar.Action icon = "dots-vertical" onPress = {handleMenu}/>
+            <Appbar.Action icon = "dots-vertical"/>
         </Appbar.Header>
     )
 }
