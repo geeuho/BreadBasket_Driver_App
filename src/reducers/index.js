3ec802cc63bfd84173621393121acad08751b7ac
geeuho <geeuho@gmail.com>
2020-09-22 15:05:10 -0700

    added reducer folder with index

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+import { reducer as formReducer } from 'redux-form'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['items', 'stores']
+}
+
+const reducers = combineReducers({
+    stores: storesReducer,
+    items: itemsReducer
+})
+
+export default persistReducer(persistConfig, reducer)
