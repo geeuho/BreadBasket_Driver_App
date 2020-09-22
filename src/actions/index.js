19b388dbe6c8b856a1460ed505707655a6fca1a1
geeuho <geeuho@gmail.com>
2020-09-22 15:02:05 -0700

    added get actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,55 @@
 import rails from '../services/Rails'
 
-// DRIVERS
+//AUTH
 
+export const signIn = (userInfo) => async dispatch => {
+    const response = await rails.post('/login', { shopper: userInfo })
+    let data = response.data
+    localStorage.setItem('shopper_token', data.jwt)
+    console.log(data.shopper.data.attributes)
+    dispatch({
+        type: 'SIGN_IN',
+        payload: data.shopper.data.attributes
+    })
+}
+
+export const signOut = () => {
+    localStorage.removeItem('shopper_token')
+    return {
+        type: 'SIGN_OUT'
+    }
+}
 
 // STORES 
 
-export const 
+export const getStores = () => async dispatch => {
+    const response = await rails.get(`/stores`)
+    dispatch({ type: 'GET_STORES', payload: response.data.data })
+}
 
 // ORDERS
 
+export const getActiveOrders = shopperId => async dispatch => {
+    const response = await rails.get(`/orders?shopper_id=${shopperId}&status=active`)
+    let data = response.data.data
+    dispatch({ type: "GET_ACTIVE_ORDERS", payload: data })
+}
+
+
 // ORDER_ITEMS 
 
+export const getOrderItems = (order_id) => async dispatch => {
+    const response = await rails.get(`order_items?order_id=${order_id}`)
+    console.log(response.data)
+    dispatch({ type: "GET_ORDER_ITEMS", payload: response.data.data })
+}
+
+
 // ITEMS
+
+export const getItems = (store_id) => async dispatch => {
+    const response = await rails.get(`/items?store_id=${store_id}`)
+    console.log(response.data, "items")
+    dispatch({ type: 'GET_ITEMS', payload: response.data })
+}
 
