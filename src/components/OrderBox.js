c185571aee4ecfac4e0af7929973091cd8c4beb1
Allen <jihoshin.28@gmail.com>
2021-02-23 20:06:29 -0800

    styling backbone for order box

--- src/components/OrderBox.js
+++ src/components/OrderBox.js
@@ -1,21 +1,62 @@
 import React from 'react'
-import { Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, TouchableOpacity, View, Image } from 'react-native'
 import { Card } from 'react-native-paper'
 
 const OrderBox = props => {
     return(
         <Card style = {styles.card}>
-            <Card.Title title={props.title}></Card.Title>
+            <Card.Title titleStyle = {styles.cost} title={props.title}></Card.Title>
             <Card.Content>
-                {props.children}
+                <View style = {styles.delivery_info}>
+                    <View>
+                        <Image></Image>
+                        <Text>8.1 miles</Text>
+                    </View>
+                    <View>
+                        <Text>7 Items/8 Units</Text>    
+                    </View>
+                </View>
+                <View style = {styles.border}></View>
+                <View style = {styles.store_info}>
+                    <View>
+                        
+                    </View>
+                    <View>
+                        <Text style = {styles.store_name}>Store City</Text>
+                        <Text>Store Address</Text>
+                    </View>
+                </View>
             </Card.Content>
         </Card>
     )
 }
 
 let styles = StyleSheet.create({
+    store_info: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    delivery_info: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     card: {
-        margin: 10
+        margin: 15,
+        padding: 10,
+        paddingTop: 20
+    },
+    store_name: {
+        fontSize: 20,
+    },
+    cost: {
+        paddingTop: 20,
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    border: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'black'
     }
 })
 
--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from "../navigation/Header"
-import Box from '../components/Box'
+import OrderBox from '../components/OrderBox'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
 class OrderScreen extends React.Component {
@@ -11,9 +11,8 @@ class OrderScreen extends React.Component {
         return(
             <View>
                 <Header navigation={this.props.navigation} title = {"Active Orders"}/>
-                <Box>
-                    
-                </Box>
+                <OrderBox title = "$13.85"/> 
+                <OrderBox title = "$24.35"/>
                 <Text style = {styles.textStyle}>Orders</Text>
                 <TouchableOpacity onPress={() => 
                     this.props.navigation.navigate('Home')
