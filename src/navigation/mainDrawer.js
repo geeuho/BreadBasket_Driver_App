7ccd697f30971fdbf56514c591f26da568927ce6
Allen <jihoshin.28@gmail.com>
2021-02-26 00:05:49 -0800

    added backbone for earnings drawer

--- src/navigation/mainDrawer.js
+++ src/navigation/mainDrawer.js
@@ -6,6 +6,10 @@ import ProfileScreen from "../screens/ProfileScreen";
 import OrdersScreen from "../screens/OrdersScreen";
 import SettingsScreen from "../screens/SettingsScreen"
 import EarningsScreen from "../screens/EarningsScreen"
+
+import earningsDrawer from './earningsDrawer'
+import orderDrawer from './orderDrawer'
+
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
--- src/navigation/orderDrawer.js
+++ src/navigation/orderDrawer.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { createDrawerNavigator } from "@react-navigation/drawer"
 
-import EarningsScreen from "../screens/EarningsScreen";
-// import PaymentMethodScreen from "../screens/PaymentMethodScreen";
-// import PaymentCardScreen from "../screens/PaymentCardScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+// import OrderHistoryScreen from "../screens/OrderHistoryScreen";
+// import OrderTutorialScreen from "../screens/OrderTutorialScreen";
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 const Drawer = createDrawerNavigator();
 
-const earningsDrawer = () => {
+const orderDrawer = () => {
     return(
         <Drawer.Navigator initialRouteName = "Home">
-            <Drawer.Screen name = "Home" component = {HomeScreen} 
+            <Drawer.Screen name = "Current Orders" component = {HomeScreen} 
                 options = {{ 
                     drawerIcon: () => { return <Icon name = "home" size = {25}/>}
                 }}
             />
-            <Drawer.Screen name = "Orders" component={OrdersScreen} 
+            <Drawer.Screen name = "Order History" component={OrdersScreen} 
                 options = {{ 
                     drawerIcon: () => { return <Icon name = "cart" size = {25}/>},
                     drawerLockMode: 'unlocked'
                 }}
             />
-            <Drawer.Screen name = "Profile" component={ProfileScreen}
+            <Drawer.Screen name = "Order Tutorial" component={ProfileScreen}
                 options = {{ 
                     drawerIcon: () => { return <Icon name = "account" size = {25}/>}
                 }}
@@ -33,4 +33,4 @@ const earningsDrawer = () => {
     )
 }
 
-export default earningsDrawer
+export default orderDrawer
