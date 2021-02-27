1dcdeb4c0175038c6ea5f8695646382d6127be71
Allen <jihoshin.28@gmail.com>
2021-02-27 01:33:51 -0800

    separated shop bar

---
+++ src/components/ShopBar.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import {Text, View, FlatList, StyleSheet, TouchableOpacity} from 'react-native'
+
+class ShopBar extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            selected: null
+        }
+    }
+
+    pressTab(value){
+        this.setState({
+            selected: value
+        })
+        this.props.changeItems(value)
+    }
+    renderTabText(value){
+        if(value === this.state.selected){
+            return {
+                fontSize: 20,
+                color: 'green'
+            }
+        } else {
+            return {
+                fontSize: 20,
+                color: 'gray'
+            }
+        }
+    }
+
+    renderTabStyle(value){
+        if(value === this.state.selected){
+            return {
+                width: 139,
+                borderBottomWidth: 2,
+                borderColor: 'green',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingBottom: 12
+            }
+        } 
+    }
+
+    render(){
+        return (
+            <View style = {styles.tabsView}>
+                    <TouchableOpacity onPress = {() => this.pressTab('todo')}>
+                        <View style = {styles.tab}>
+                            <View style = {this.renderTabStyle('todo')}>
+                                <Text style = {this.renderTabText('todo')}>
+                                    {this.props.orderCount} To-do
+                                </Text>    
+                            </View>
+                        </View>
+                    </TouchableOpacity> 
+                    <TouchableOpacity onPress = {() => this.pressTab('pending')}>
+                        <View style = {styles.tab}> 
+                            <View style = {this.renderTabStyle('pending')}>
+                                <Text style = {this.renderTabText('pending')}>
+                                    Pending
+                                </Text>    
+                            </View> 
+                        </View>  
+                    </TouchableOpacity> 
+                    <TouchableOpacity onPress = {() => this.pressTab('completed')}>
+                        <View style = {styles.tab}>
+                            <View style = {this.renderTabStyle('completed')}>
+                                <Text style = {this.renderTabText('completed')}>
+                                    Completed
+                                </Text>    
+                            </View>   
+                        </View>
+                    </TouchableOpacity> 
+                </View>
+        )
+
+    }
+}
+
+let styles = StyleSheet.create({
+    tab: {
+        width: 138,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+        
+    },
+    tabText: {
+        color: 'gray',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    tabsView: {
+        display: "flex",
+        flexDirection: "row",
+
+    },
+})
+
+export default ShopBar
--- src/screens/OrderShopScreen.js
+++ src/screens/OrderShopScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Text, View, FlatList, StyleSheet, TouchableOpacity} from 'react-native'
 import {connect} from 'react-redux'
 import Header from '../header/Header'
+import ShopBar from '../components/ShopBar'
 
 class OrderShopScreen extends React.Component {
 
@@ -12,43 +13,20 @@ class OrderShopScreen extends React.Component {
     constructor(){
         super()
         this.state = {
-            selected: null
+            renderedItems: []
         }
     }
 
-    renderTabText(value){
-        if(value === this.state.selected){
-            return {
-                fontSize: 18,
-                color: 'green'
-            }
-        } else {
-            return {
-                fontSize: 18,
-                color: 'gray'
-            }
-        }
+    changeItems = (value) => {
+        console.log(value)
     }
 
-    renderTabStyle(value){
-        if(value === this.state.selected){
-            return {
-                width: 139,
-                borderBottomWidth: 2,
-                borderColor: 'green',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                paddingBottom: 15
-            }
-        } 
-    }
+    renderItems = () => {
+        return(
+            <FlatList>
 
-    pressTab(value){
-        this.setState({
-            selected: value
-        })
+            </FlatList>
+        ) 
     }
 
 //Need to create a tab bar for each of the item sections
@@ -60,64 +38,15 @@ class OrderShopScreen extends React.Component {
         return(
         <View>
             <Header icon = "menu" styles = {{'backgroundColor': '#98fb98'}} navigation={this.props.navigation} />
-            <View style = {styles.tabsView}>
-                <TouchableOpacity onPress = {() => this.pressTab('todo')}>
-                    <View style = {styles.tab}>
-                        <View style = {this.renderTabStyle('todo')}>
-                            <Text style = {this.renderTabText('todo')}>
-                                To-do
-                            </Text>    
-                        </View>
-                    </View>
-                </TouchableOpacity> 
-                <TouchableOpacity onPress = {() => this.pressTab('pending')}>
-                    <View style = {styles.tab}> 
-                        <View style = {this.renderTabStyle('pending')}>
-                            <Text style = {this.renderTabText('pending')}>
-                                Pending
-                            </Text>    
-                        </View> 
-                    </View>  
-                </TouchableOpacity> 
-                <TouchableOpacity onPress = {() => this.pressTab('completed')}>
-                    <View style = {styles.tab}>
-                        <View style = {this.renderTabStyle('completed')}>
-                            <Text style = {this.renderTabText('completed')}>
-                                Completed
-                            </Text>    
-                        </View>   
-                    </View>
-                </TouchableOpacity> 
-            </View>
+            <ShopBar changeItems = {this.changeItems} orderCount = {this.props.currentOrder.order_count}/>
+            {this.renderItems()}
         </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    header: {
-        fontWeight: 'bold',
-        fontSize: 30
-    },
-    tabsView: {
-        display: "flex",
-        flexDirection: "row",
-
-    },
-    tab: {
-        width: 138,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20
-        
-    },
-    tabText: {
-        color: 'gray',
-        fontWeight: 'bold',
-        fontSize: 20
-    }
+    
 })
 
 let mapStateToProps = state => {
